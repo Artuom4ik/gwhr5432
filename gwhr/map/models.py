ac639af5bb209415310fb8a8f33a9823369d7bb3
Artuom4ik <masterskayaigr123@gmail.com>
2025-04-30 12:04:24 +0300

    Create Place model

--- gwhr/map/models.py
+++ gwhr/map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    coordinates = models.JSONField(verbose_name='Координаты')
+    additional_data = models.JSONField(default=dict, verbose_name='Дополнительные данные')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создание')
+
+    def __str__(self):
+        return self.name
