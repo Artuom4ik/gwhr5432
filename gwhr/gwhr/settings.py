fd3b7e8db60410aa581452154f40a07b8a31e50f
Artuom4ik <masterskayaigr123@gmail.com>
2025-04-30 11:51:54 +0300

    Create map app and add GDAL,GEOS path

--- gwhr/gwhr/settings.py
+++ gwhr/gwhr/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leaflet',
+    'map.apps.MapConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GDAL_LIBRARY_PATH = env('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = env('GEOS_LIBRARY_PATH')
